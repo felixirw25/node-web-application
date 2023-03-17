06f9963815b3ccfb0aeb76c5cbe7b36edf73d24f
Felix Irwanto <felixirwanto25@gmail.com>
2023-03-17 14:00:42 +0800

    Subdomain

--- app.js
+++ app.js
@@ -8,5 +8,9 @@ app.get('/', (req,res) => {
     res.send('Hello World - Node.js');
 })
 
+app.get('/mca', (req,res) => {
+    res.send('Hello World MCA - Node.js');
+})
+
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));

f90ef28efdcde143cd7d25f1ffb8de4c4c1292d5
Felix Irwanto <felixirwanto25@gmail.com>
2023-03-17 14:11:10 +0800

    Update Subdomain

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ app.get('/', (req,res) => {
     res.send('Hello World - Node.js');
 })
 
-app.get('/mca', (req,res) => {
-    res.send('Hello World MCA - Node.js');
-})
+// app.get('/mca', (req,res) => {
+    
+// })
 
+app.get('*', function(req, res, next){ 
+    if(req.headers.host == 'mca.herokufi.online')  //if it's a sub-domain
+      req.url = '/mca';  //append some text yourself
+      res.send('Hello World MCA - Node.js');
+    next(); 
+  });
+  
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
